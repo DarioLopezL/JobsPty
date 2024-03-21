4eab569cf14725132344b0d4bfaf83f4d5c2babe
DarioLopezL <dariiolopez17@gmail.com>
2024-03-21 16:07:06 -0600

    add commit public vacante components livewire, models and migrations

--- app/Livewire/CrearVacante.php
+++ app/Livewire/CrearVacante.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use App\Models\Categoria;
 use App\Models\Salario;
+use App\Models\Vacante;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 
@@ -31,7 +32,33 @@ class CrearVacante extends Component
 
     public function CrearVacante()
     {
-        $this->validate();
+         $datos= $this->validate();
+
+
+        //almacenar la imagen
+        $imagen = $this->imagen->store('public/vacantes');
+        $datos['imagen'] = str_replace('public/vacantes','', $imagen);
+
+
+
+        //crear la vacante
+        Vacante::create([
+            'titulo'=> $datos['titulo'],
+            'salario_id'=>$datos['salario'],
+            'categoria_id'=>$datos['categoria'],
+            'empresa'=>$datos['empresa'],
+            'ultimo_dia'=>$datos['ultimo_dia'],
+            'descripcion'=>$datos['descripcion'],
+            'imagen'=>$datos['imagen'],
+            'user_id'=> auth()->user()->id,
+        ]);
+
+        //crear un mensaje
+        session()->flash('mensaje','La vacante se publicó correctamente');
+
+        //redireccionar al usuario
+        return redirect()->route('vacantes.index');
+
     }
 
 
